f1124a792d46c56fa7573d9bddecc7f6bc850fb6
afraj18 <afraj.2011@gmail.com>
2021-09-18 21:47:38 +0530

    2ND COMMIT

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,30 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
+import { uuid } from 'uuidv4';
 
 function App() {
   const [contacts, setContacts] = useState([]);
-  const addContactHandler = (contacts) => {
-    console.log(contacts);
+  const LOCAL_STORAGE_KEY = "contacts";
+
+  const addContactHandler = (contact) => {
+    // console.log(contacts);
+    setContacts([...contacts, { id: uuid, ...contact }]);
   }
+
+  useEffect(() => {
+    const retrContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retrContacts) setContacts(retrContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+  }, [contacts]);
+
+
   return (
     <div className="ui container">
       <Header />
